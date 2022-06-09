e69cf7d4712034d8247c9acfdec1aebcf2332d16
penghui.pan <Penghui.Pan@tpv-tech.com>
2022-06-09 11:39:47 +0800

    write some test file

---
+++ test1.py
@@ -0,0 +1,10 @@
+def outer(a):
+    b = 10
+    # inner是内函数
+    def inner():
+        # 在内函数中 用到了外函数的临时变量
+        print(a + b)
+    # 外函数的返回值是内函数的引用
+    return inner
+if __name__ == "__main__":
+    outer(5)()
